57b14b07689fe46fbd85c9dfcd861a04fd580c7a
Seyfaddin Nacafli <97394992+coderseyfi@users.noreply.github.com>
2022-02-26 11:38:22 +0400

    Update Program.cs

--- Solution-15/Program.cs
+++ Solution-15/Program.cs
@@ -6,7 +6,58 @@ namespace Solution_15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+              //            15)*Verilmish metinde ilk ve son simvol eynidirse,
+            //ve metn daxilinde yanashi gelen { a}
+            //            simvolu varsa,
+            //ve metn daxilinde { b}
+            //            simvolu yoxdursa
+            //o zaman bu metnde butun { c}
+            //            simvollari yox et ve
+            //neticede alinan metn zerkalni olub olmadigini yoxla.
+
+
+            string txt = Console.ReadLine();
+            char a = 'a';
+            char b = 'b';
+            char c = 'c';
+            char first = txt.FirstOrDefault();
+            char last = txt.LastOrDefault();
+            bool check = false;
+            
+
+            for (int i = 0; i < txt.Length-2; i++)
+            {
+                if (first == last )
+                {
+                    if (txt[i]== a && txt[i+1]== a)
+                    {
+                        if (txt[i] !=b)
+                        {
+                            txt = txt.Replace(c.ToString(), "");
+                           
+
+                            check = true;
+                        }
+                    }
+                }
+            }
+
+
+            if (check == true)
+            {
+                Console.WriteLine("metn zerkalnidi");
+            }
+            else
+            {
+                Console.WriteLine("metn o deyil");
+            }
+
+            
+            Console.WriteLine(txt);
+            Console.WriteLine(check);
+            
+            
+            
         }
     }
 }
